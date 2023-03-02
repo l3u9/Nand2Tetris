24cb2b12636a54d6b21a60a1bd627e5985d498ec
l3u9 <ksj31292000@gmail.com>
2023-03-02 23:01:56 +0900

    chapter 4 mul clear

--- Project/04/mult/Mult.asm
+++ Project/04/mult/Mult.asm
@@ -10,3 +10,30 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+
+    @2
+    M=0
+    @i
+    M=0
+(LOOP)
+    @i
+    D=M
+    @1
+    D=D-M
+    @END
+    D; JGE
+    
+    @0
+    D=M
+    @2
+    M=D+M
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
+
